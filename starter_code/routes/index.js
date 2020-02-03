e538db91222e38ed518717d812f659f5f66f9e3d
KARO <karolina.czarnowska@gmail.com>
2020-02-03 18:44:07 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,30 @@
-const express = require('express');
-const router  = express.Router();
-
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie.js");
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (request, respond, next) => {
+  Movie.find({})
+    .then(movieFile => {
+      // respond.send(movieFile);
+      respond.render("movies.hbs", { movieList: movieFile });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.get("/movies/:id", (request, response, next) => {
+  Movie.findById(request.params.id)
+    .then(movieFile => {
+      response.render("movieDetails.hbs", movieFile);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = router;
